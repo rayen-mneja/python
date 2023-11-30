b1523d8139b427b374e96b725c6926351ed20273
sangour <149207012+RayenMneja@users.noreply.github.com>
2023-11-30 22:44:44 +0100

    up

--- table/Untitled-1.py
+++ table/Untitled-1.py
@@ -7,36 +7,38 @@ def saisir_dimontion():
     while not(2<=NL<=20 and 1<=NC<=20):
         NL=int(input('NL='))
         NC=int(input('NC='))
+    NC+=1
 #kol ligne fih des colone mechi na3mlou il parcour mte3hom
 def romplir_MAT(M,NL,NC):
     #il for loula bech na3mou il parcour mte3 les lignes
     for i in range(NL):
-        #il for il theniya bech na3mlou il parcour mte3 les colones
-        for j in range(NC):
+        #il for il theniya bech na3mlou il parcour mte3 les colones fi kol ligne
+        for j in range(NC-1):
             M[i,j]=int(input('M['+str(i+1)+','+str(j+1)+']='))
-            while not(0<=M[i,j]<=20):
+            while not(0<=M[i,j]<=20):#condition
                 M[i,j]=int(input('M['+str(i+1)+','+str(j+1)+']='))
-def somme(M,NL,NC,t):
+#bech na3mlou il somme mte3 les colones fi kol linge
+def somme(M,NL,NC):
     for i in range(NL):
+        #kol mara ye7seb les colone mte3 
         s=0
-        for j in range(NC):
+        for j in range(NC-1):
             s+=M[i,j]
-        t[i]=s
-def affiche(M,NL,NC,t):
+        M[i,NC-1]=s
+def affiche(M,NL,NC):
     for i in range(NL):
         ch=''
-        for j in range(NC):
-            if j<NC-1:
+        for j in range(NC-1):
+            if j<NC-2:
                 ch=ch+str(M[i,j])+"+"
             else:
                 ch=ch+str(M[i,j])
-        print(ch,'=',str(t[i]))
+        print(ch,'=',str(M[i,NC-1]))
             
 
 #APP
 saisir_dimontion()
 M=array([[int()]* NC for i in range(NL)])
-t=array([int]*NL)
 romplir_MAT(M,NL,NC)
-somme(M,NL,NC,t)
-affiche(M,NL,NC,t)
+somme(M,NL,NC)
+affiche(M,NL,NC)
