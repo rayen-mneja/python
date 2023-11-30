d971578897b27be5368dbfe2dd2d37e2a0738790
sangour <149207012+RayenMneja@users.noreply.github.com>
2023-11-30 22:29:05 +0100

    tafsir 1

--- table/Untitled-1.py
+++ table/Untitled-1.py
@@ -1,16 +1,20 @@
 from numpy import*
+#il NL=nombre de ligne / NC=nombre de colone
 def saisir_dimontion():
     global NL,NC
     NL=int(input('NL='))
     NC=int(input('NC='))
-    while not(4<=NL<=20 and 10<=NC<=20):
+    while not(2<=NL<=20 and 1<=NC<=20):
         NL=int(input('NL='))
         NC=int(input('NC='))
+#kol ligne fih des colone mechi na3mlou il parcour mte3hom
 def romplir_MAT(M,NL,NC):
+    #il for loula bech na3mou il parcour mte3 les lignes
     for i in range(NL):
+        #il for il theniya bech na3mlou il parcour mte3 les colones
         for j in range(NC):
             M[i,j]=int(input('M['+str(i+1)+','+str(j+1)+']='))
-            while M[i,j]>20 or M[i,j]<0:
+            while not(0<=M[i,j]<=20):
                 M[i,j]=int(input('M['+str(i+1)+','+str(j+1)+']='))
 def somme(M,NL,NC,t):
     for i in range(NL):
@@ -31,7 +35,7 @@ def affiche(M,NL,NC,t):
 
 #APP
 saisir_dimontion()
-M=array([[int()]*NL for i in range(NC)])
+M=array([[int()]* NC for i in range(NL)])
 t=array([int]*NL)
 romplir_MAT(M,NL,NC)
 somme(M,NL,NC,t)
